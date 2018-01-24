a20048b4a5aca17776b4327bba3865f289711dea
AnalogMemory <alex@analogmemory.xyz>
2018-01-24 02:19:23 -0800

    Testing netflify contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,20 @@ const ContactPage = props => {
   return (
     <div>
       <h1>{page.title}</h1>
+      <form action={`/contact/?success=true`} name={`contact`} data-netlify={`true`}>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   )
 }
