9fe71d0c03e5370444646f4fd99b9559f481e0f3
AnalogMemory <alex@analogmemory.xyz>
2018-02-22 01:45:52 -0800

    Progress on Works page * Layout content from contentful * Stub out styles

--- src/pages/works.js
+++ src/pages/works.js
@@ -5,40 +5,105 @@ import Title from '../components/TitleEl'
 
 const Section = styled.section`
   width: 100%;
-  margin: 100px 0 0;
-  background: #fff;
+  min-height: 100%;
+  margin: 0;
+  padding: 2em;
 `
 const WorkCardWrap = styled.div`
   width: 100%;
-  max-width: 75rem;
+  background: #fff;
   margin: 0 auto;
   display: flex;
-  justify-content: center;
+  flex-wrap: wrap;
+  border: 2em solid #fff;
+  border-top: none;
 `
 const WorkCard = styled.div`
-  max-width: 33.33%;
+  max-width: 50%;
   width: 100%;
-  flex-basis: 33.33%;
-  border: 1px solid red;
+  flex-basis: 50%;
+  position: relative;
+  overflow: hidden;
+
+  &:hover {
+    figure {
+      transform: scale(1.08);
+    }
+  }
+
+  figure {
+    margin: 0;
+    width: 100%;
+    height: 0;
+    padding-bottom: 56.25%;
+    overflow: hidden;
+    transition: transform 400ms ease;
+
+    img {
+      width: 100%;
+      height: auto;
+      display: block;
+      object-fit: cover;
+      position: absolute;
+      top: 0;
+    }
+  }
+
+  .copy {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    padding: 2em;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-content: flex-end;
+    background: linear-gradient(to bottom, rgba(0,0,0,0) 40%, rgba(0,0,0,0.85) 90%);
+
+    h3, p, a {
+      color: #fff;
+      margin-bottom: 0;
+    }
+
+    h3 {
+      margin-bottom: 0.25em;
+    }
+
+    p {
+      color: #a5a5a5;
+    }
+  }
+`
+const TitleHeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50%;
+  position: relative;
+  z-index: 3;
+  border: 2em solid #fff;
 `
 
 const Works = props => {
   const works_data = props.data.allContentfulWorks.edges
   return (
     <Section>
-      <Title text={`Works`} size={`h1`} />
+      <TitleHeader>
+        <Title text={`Works`} size={`h1`} color={`#fff`} margin={`auto`} />
+      </TitleHeader>
       <WorkCardWrap>
         {works_data.map(({ node: work }) => (
           <WorkCard key={work.id}>
             <figure>
               <img src={work.featuredImage.sizes.src} alt="" title={work.title}/>
             </figure>
-            <h3>
-              <Link to={`/works/${work.slug}/`}>{work.title}</Link>
-            </h3>
-            <p>
-              {work.description.description}
-            </p>
+            <Link className="copy" to={`/works/${work.slug}/`}>
+              <h3>{work.title}</h3>
+              <p>{work.description.description}</p>
+            </Link>
           </WorkCard>
         ))}
       </WorkCardWrap>
@@ -57,7 +122,7 @@ export const worksQuery = graphql`
           title
           slug
           featuredImage {
-            sizes(maxWidth: 600) {
+            sizes(maxWidth: 1000, quality: 60) {
               src
             }
           }
