db3bed5564c29c20787b3a17322ac7e491090089
AnalogMemory <alex@analogmemory.xyz>
2018-02-22 01:45:02 -0800

    Update layout for page wrapper

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 import Header from '../components/Header'
 import 'normalize.css'
 import '../css/base.scss'
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`
+
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <Wrapper>
     <Helmet title="Analog Memory — Front-End Development"
             meta={[
               { name: 'description', content: 'Portfolio Website of Analog Memory. Front-End Developer based in San Francisco' },
             ]} />
     <Header/>
     {children()}
-  </div>
+  </Wrapper>
 )
 
 TemplateWrapper.propTypes = {
