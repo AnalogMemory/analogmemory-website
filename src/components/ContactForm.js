3f4f86b4cec285e124d2de4fe12d6e66b077f5e8
AnalogMemory <alex@analogmemory.xyz>
2018-02-27 17:49:20 -0800

    Adds active state functionality for each form element

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,7 +8,7 @@ const Form = styled.form`
 const FieldGroup = styled.div`
   position: relative;
   width: 100%;
-  margin: 2rem 0;
+  margin: 2rem 0 2rem;
 `
 const Label = styled.label`
   color: #5c5f60;
@@ -19,40 +19,73 @@ const Label = styled.label`
   left: 0;
   top: 0.375rem;
   transition: all 300ms linear;
+
+  &:hover {
+    color: #114357;
+  }
+
+  ${props => props.isActive && `
+    font-size: 0.875rem;
+    transform: translateY(-115%);
+    color: #bbbbbb;
+  `}
 `
 const Input = styled.input`
   background-color: transparent;
   border: none;
-  border-bottom: 3px solid #dddddd;
+  border-bottom: 2px solid #dddddd;
   box-shadow: none;
   color: #131516;
   height: 3rem;
   outline: none;
   font-size: 1.125rem;
-  margin-bottom: 0.25rem;
   line-height: normal;
   padding: 0;
-  transition: box-shadow 300ms linear;
+  transition: border 300ms linear, box-shadow 300ms linear;
   width: 100%;
+
+  ${props => props.isActive && `
+    border-color: #114357;
+    box-shadow: 0 2px 0 0 #114357;
+    outline: none;
+  `}
 `
 const Textarea = Input.withComponent(`textarea`)
+const TextareaStyled = Textarea.extend`
+  padding: 1em 0;
+  height: auto;
+  color: #131516;
+`
 
 class ContactForm extends Component {
+  state = {
+    activeInput: null
+  }
+
+  handleFocus = (inputName) => {
+    this.setState({ activeInput: inputName })
+  }
+
+  handleBlur = () => {
+    this.setState({ activeInput: null })
+  }
+
   render () {
+    const { activeInput } = this.state
     return (
       <Form>
         <Title text={`Let’s Talk`} size={`h4`} />
         <FieldGroup>
-          <Label>Name</Label>
-          <Input type="text" />
+          <Label isActive={activeInput === `name`}>Name</Label>
+          <Input type="text" name="name" isActive={activeInput === `name`} onFocus={() => this.handleFocus(`name`)} onBlur={this.handleBlur} />
         </FieldGroup>
         <FieldGroup>
-          <Label>Email</Label>
-          <Input type="email" />
+          <Label isActive={activeInput === `email`}>Email</Label>
+          <Input type="email" name="email" isActive={activeInput === `email`} onFocus={() => this.handleFocus(`email`)} onBlur={this.handleBlur} />
         </FieldGroup>
-        <FieldGroup>
-          <Label>Message</Label>
-          <Textarea></Textarea>
+        <FieldGroup index={2}>
+          <Label isActive={activeInput === `message`}>Message</Label>
+          <TextareaStyled name="message" isActive={activeInput === `message`} onFocus={() => this.handleFocus(`message`)} onBlur={this.handleBlur}></TextareaStyled>
         </FieldGroup>
       </Form>
     )
