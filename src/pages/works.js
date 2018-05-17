29ee519ca63494de739010a34f31eee8f50f9c00
AnalogMemory <alex@analogmemory.xyz>
2018-05-17 02:43:04 -0700

    Update works page layout

--- src/pages/works.js
+++ src/pages/works.js
@@ -10,12 +10,11 @@ const Section = styled.section`
 `
 const WorkCardWrap = styled.div`
   width: 100%;
-  background: #fff;
+  background: #f6faf9;
   margin: 0 auto 0;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  border: 2em solid #fff;
   border-top: none;
   position: relative;
   padding: 0;
@@ -26,93 +25,37 @@ const WorkCard = styled.div`
   width: 100%;
   margin: 0 -0.75rem;
 
-  &:hover {
-    .copy {
-
-      h3 {
-        transform: translateY(0);
-        ${props => props.offset && `margin-bottom: 0.25em;`}
-      }
-      p {
-        opacity: 1;
-        transform: translateY(0);
-      }
-      span {
-        opacity: 0.8;
-        transform: translateY(0%) skewY(-1.5deg);
-      }
-    }
-  }
-
   figure {
     margin: 0;
     width: 100%;
     height: 0;
     padding-bottom: 56.25%;
     overflow: hidden;
-    transition: transform 400ms ease;
 
     img {
       width: 100%;
       height: auto;
       display: block;
       object-fit: cover;
-      position: absolute;
-      top: 0;
     }
   }
 
   .copy {
-    position: absolute;
-    bottom: 0;
+    position: relative;
     width: 100%;
-    height: auto;
-    z-index: 2;
-    padding: 2em;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-content: flex-end;
-    background: linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.8) 90%);
-
-    h3, p, a {
-      color: #fff;
-      margin-bottom: 0;
-      position: relative;
-      z-index: 5;
-    }
-
-    h3 {
-      transform: translateY(75%);
-      transition: transform 300ms ease;
-
-      ${props => props.offset == true && `
-        margin-bottom: 0;
-        transform: translateY(78%);
-        transition: transform 300ms ease-out, margin 300ms ease-out;
-      `}
-    }
+    padding: 1em 0;
 
     p {
-      color: #a5a5a5;
-      opacity: 0;
-      font-size: 1em;
-      transform: translateY(75%);
-      transition: transform 300ms ease, opacity 300ms 100ms ease;
+      margin-bottom: 0.25rem;
     }
 
-    span {
-      display: block;
-      position: absolute;
-      width: 100%;
-      height: 200%;
-      top: 0;
-      left: 0;
-      z-index: 1;
-      background-color: #000000;
-      opacity: 0;
-      transform: translateY(10%);
-      transition: transform 300ms ease-out, opacity 300ms ease-out;
+    .project-type {
+      color: #707C80;
+    }
+
+    .project-tech {
+      font-size: 0.875rem;
+      color: #b0bec5;
     }
   }
 `
@@ -157,15 +100,27 @@ const Works = props => {
           <WorkCard key={work.id} offset={work.role && work.role.length > 0}>
             <WorkCardInner>
               <figure>
-                <img src={work.featuredImage.sizes.src} alt="" title={work.title}/>
+                <Link to={`/works/${work.slug}/`}>
+                  <img src={work.featuredImage.sizes.src} alt="" title={work.title}/>
+                </Link>
               </figure>
-              <Link className="copy" to={`/works/${work.slug}/`}>
-                <h3 dangerouslySetInnerHTML={{ __html: work.title }} />
-                {work.projectType && (
-                  <p dangerouslySetInnerHTML={{ __html: work.projectType.join(', ') }} />
-                )}
-                <span></span>
-              </Link>
+              <div className="copy">
+                <h3>
+                  <Link to={`/works/${work.slug}/`}>
+                    {work.title}
+                  </Link>
+                </h3>
+                {work.projectType &&
+                  <p
+                    className="project-type"
+                    dangerouslySetInnerHTML={{ __html: work.projectType.join(', ') }} />
+                }
+                {work.projectTech &&
+                  <p
+                    className="project-tech"
+                    dangerouslySetInnerHTML={{ __html: work.projectTech.join(', ') }} />
+                }
+              </div>
             </WorkCardInner>
           </WorkCard>
         ))}
@@ -192,6 +147,7 @@ export const worksQuery = graphql`
             }
           }
           projectType
+          projectTech
         }
       }
     }
